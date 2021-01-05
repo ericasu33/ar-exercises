632c947c876d49e94fdd8e1787bf4161ddbca74b
Erica <ericasu33@gmail.com>
2021-01-05 00:43:46 +0000

    exercise7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,13 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 30)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 80)
 @store1.employees.create(first_name: "Tom", last_name: "Smith", hourly_rate: 40)
 @store1.employees.create(first_name: "Larry", last_name: "Smith", hourly_rate: 60)
-@store1.employees.create(first_name: "Kent", last_name: "Smith", hourly_rate: 30)
+@store1.employees.create(first_name: "Kent", last_name: "Smith", hourly_rate: 40)
 
 @store2.employees.create(first_name: "Kate", last_name: "Smith", hourly_rate: 40)
-@store2.employees.create(first_name: "Cat", last_name: "Smith", hourly_rate: 30)
-@store2.employees.create(first_name: "Apple", last_name: "Smith", hourly_rate: 20)
+@store2.employees.create(first_name: "Cat", last_name: "Smith", hourly_rate: 80)
+@store2.employees.create(first_name: "Apple", last_name: "Smith", hourly_rate: 90)
 @store2.employees.create(first_name: "Adrea", last_name: "Smith", hourly_rate: 50)
-@store2.employees.create(first_name: "Anita", last_name: "Smith", hourly_rate: 30)
+@store2.employees.create(first_name: "Anita", last_name: "Smith", hourly_rate: 70)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please input a store name"
+
+storeName = gets.chomp
+
+newStore = Store.create(name: storeName)
+
+puts newStore.errors.full_messages
+
+employee1 = @store1.employees.create(last_name: "Smith", hourly_rate: 80)
+
+puts employee1.errors.full_messages
